159d47dd53d8990de22d4db15df85471cc3ad14e
Anton S <spaxi4@yandex.ru>
2024-10-03 20:17:22 +0400

    removed test

--- src/pages/tasks/[filter]/index.astro.test.js
+++
@@ -1,9 +0,0 @@
-import { experimental_AstroContainer as AstroContainer } from 'astro/container';
-import { expect, test } from 'vitest';
-import IndexPage from './index.astro';
-
-// TODO: No valid renderer was found for the `.tsx` file extension.
-test('Index page', async () => {
-  // const container = await AstroContainer.create();
-  // const result = await container.renderToString(IndexPage, {});
-});
